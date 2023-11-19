1f41be71944f0f78e1ff98a058ee1dbc04fd8d3d
Ashish Bhandari <officialashish555@gmail.com>
2023-11-19 13:58:31 +0530

    multer and cloudinary

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
     statusCode,
     message = "Something went wrong",
     error = [],
-    statck = ""
+    stack = ""
   ) {
     super(message);
     this.statusCode = statusCode;
@@ -12,7 +12,7 @@ class ApiError extends Error {
     this.success = false;
     this.errors = this.errors;
 
-    if (statck) {
+    if (stack) {
       this.stack = stack;
     } else {
       Error.captureStackTrace(this, this.constructor);
